b55993a1201ce42148e4636cf6a61b1afd274da3
ko153 <kovendan153@gmail.com>
2024-10-15 21:21:31 +0530

    Integrate weather API to fetch current temperature data

---
+++ script.js
@@ -0,0 +1,10 @@
+const apiKey = "";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=delhi";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+checkWeather();
